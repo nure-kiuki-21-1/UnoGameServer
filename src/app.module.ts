a81552d2ad7314237c8fdc278924b0082fcf9cc6
Sierov Andrii <serovandrey2004@gmail.com>
2023-12-25 12:13:07 +0200

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/user/dto/update-user.dto.ts
@@ -0,0 +1,43 @@
+import {
+  IsAlphanumeric,
+  IsEmail,
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+
+const passwordRegEx = /^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,20}$/;
+
+export class UpdateUserDto {
+  @IsString()
+  @MinLength(2, { message: 'Name must have atleast 2 characters.' })
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  @MinLength(3, { message: 'Username must have atleast 3 characters.' })
+  @IsAlphanumeric(null, {
+    message: 'Username does not allow other than alpha numeric chars.',
+  })
+  username: string;
+
+  @IsNotEmpty()
+  @IsEmail(null, { message: 'Please provide valid Email.' })
+  email: string;
+
+  @IsInt()
+  age: number;
+
+  @IsString()
+  @IsEnum(['f', 'm', 'u'])
+  gender: string;
+
+  @IsNotEmpty()
+  @Matches(passwordRegEx, {
+    message: `Password must contain Minimum 8 and maximum 20 letters or digits`,
+  })
+  password: string;
+}
